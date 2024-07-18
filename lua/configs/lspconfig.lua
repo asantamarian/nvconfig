13a3271c9dc92fcee027c846ed349ab59e4fe983
Andrés Santa María <asantnor@gmail.com>
2024-07-18 18:47:49 -0400

    lps gopls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,9 +4,20 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "gopls" }
 
 -- lsps with default config
+lspconfig.gopls.setup({
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+    },
+  },
+})
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
@@ -20,4 +31,5 @@ lspconfig.tsserver.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+  
 }
